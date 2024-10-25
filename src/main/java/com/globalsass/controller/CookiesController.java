6e9c4d07d31fcfb5e533136c5204a5bf990c13fc
Debasish Das <dasdebasish199@gmail.com>
2024-10-25 18:56:52 +0530

    25 oct

--- src/main/java/com/globalsass/controller/CookiesController.java
+++ src/main/java/com/globalsass/controller/CookiesController.java
@@ -2,9 +2,9 @@ package com.globalsass.controller;
 
 import com.globalsass.model.StyleEntity;
 import com.globalsass.service.StyleService;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseCookie;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -16,7 +16,25 @@ public class CookiesController {
     @Autowired
     private StyleService styleService;
 
-    @PostMapping("/")
+    @PostMapping("/create")
+    public String createCookie(@RequestParam("styleId") Long styleId, HttpServletResponse response) {
+        StyleEntity styleEntity = styleService.getStyleById(styleId);
+        if (styleEntity != null) {
+            ResponseCookie cookie = ResponseCookie.from("THEME_ID", styleEntity.getId().toString())
+                    .path("/")
+                    .maxAge(4800)
+                    .sameSite("None")
+                    .secure(true)
+                    .build();
+            response.addHeader("Set-Cookie", cookie.toString());
+            return "Cookie created successfully!";
+        } else {
+            return "StyleEntity not found!";
+        }
+    }
+
+
+    /*@PostMapping("/")
     public String createCookie(@RequestParam("styleId") Long styleId, HttpServletResponse response) {
         StyleEntity styleEntity = styleService.getStyleById(styleId);
         if (styleEntity != null) {
@@ -29,7 +47,7 @@ public class CookiesController {
             return "StyleEntity not found!";
         }
 
-    }
+    }*/
 
     @GetMapping("/fetch-cookies")
     public Map<String, String> fetchVariables(@CookieValue("THEME_ID") Long styleId) {
